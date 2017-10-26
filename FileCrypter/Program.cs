472fc8280912b343c5484d6efcafaf3b2c295aa0
Valsov <Valsov@DESKTOP-54TDIGM>
2017-10-26 22:34:34 +0200

    Dev v01

---
+++ FileCrypter/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileCrypter
+{
+    class Program
+    {
+        enum Methods { encrypt, decrypt }
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                ConsoleColorHelper.Init();
+
+                var path = args[0];
+                var files = new List<string>();
+
+                Methods method = PromptMethod(args[1] == "folder");
+                var password = PromptPassword(false, method);
+                bool destructFiles = PromptDestruct(args[1] == "folder");
+
+
+                Console.Write("\nTarget:  ");
+                ConsoleColorHelper.WriteLine(path, ConsoleColorHelper.Colors.File);
+                if (args[1] == "folder")
+                {
+                    Console.Write("Warning : the folder encryption / decryption is ");
+                    ConsoleColorHelper.WriteLine("RECURSIVE", ConsoleColorHelper.Colors.Important);
+                }
+                Console.WriteLine("\nAre you ready to launch the process ? (Press any key... or CTRL + C to quit)");
+                Console.Read();
+
+                if (args[1] == "file")
+                {
+                    if (File.Exists(path))
+                        files.Add(path);
+                }
+                else
+                {
+                    if (Directory.Exists(path))
+                    {
+                        foreach (var file in GetFilesInFolder(path))
+                            files.Add(file);
+                    }
+                }
+
+                foreach(var file in files)
+                {
+                    var fileInfo = new FileInfo(file);
+                    ConsoleColorHelper.Write("\n" + fileInfo.Name, ConsoleColorHelper.Colors.File);
+                    Console.Write("  ->  ");
+
+                    using (var fileStream = new FileStream(fileInfo.FullName, FileMode.Open))
+                    {
+                        try
+                        {
+                            if (method == Methods.encrypt)
+                                Security.Encrypt(fileStream, fileInfo.FullName + ".crypted", password);
+                            else
+                                Security.Decrypt(fileStream, fileInfo.FullName.Substring(0, fileInfo.FullName.LastIndexOf('.')), password);
+                        }
+                        catch (IOException)
+                        {
+                            Console.WriteLine("\nIO error... unlucky");
+                        }
+                        catch (Exception)
+                        {
+                            ConsoleColorHelper.WriteLine("\nSomething went wrong... unlucky", ConsoleColorHelper.Colors.Error);
+                        }
+                    }
+
+                    if (destructFiles)
+                        File.Delete(file);
+                }
+                
+                
+                Console.WriteLine("\n----------\nDone !");
+                Console.ReadLine();
+            }
+        }
+
+
+        static string PromptPassword(bool reType, Methods method)
+        {
+            var password = "";
+            var reTypePassword = "";
+
+            while(password == "")
+            {
+                Console.Write("\n{0} :{1}", reType ? "Type password again" : "Password", reType ? "     " : "                ");
+                var input = new ConsoleKeyInfo();
+
+                while (input.Key != ConsoleKey.Enter)
+                {
+                    input = Console.ReadKey(true);
+
+                    if (input.Key == ConsoleKey.Backspace)
+                    {
+                        if (password.Length != 0)
+                        {
+                            password = password.Substring(0, password.Length - 1);
+                            Console.Write("\b \b");
+                        }
+                    }
+                    else if (input.Key != ConsoleKey.Enter)
+                    {
+                        password += input.KeyChar;
+                        ConsoleColorHelper.Write("*", ConsoleColorHelper.Colors.Password);
+                    }
+                }
+
+                if(password == "")
+                {
+                    ConsoleColorHelper.Write("Please enter a password...", ConsoleColorHelper.Colors.Error);
+                }
+                else
+                {
+                    if (!reType)
+                    {
+                        while (password != reTypePassword && method == Methods.encrypt)
+                        {
+                            if (password != reTypePassword && reTypePassword != "")
+                                ConsoleColorHelper.Write("\nPassword don't match", ConsoleColorHelper.Colors.Error);
+
+                            reTypePassword = PromptPassword(true, method);
+                        }
+                    }
+                }
+            }            
+
+            return password;
+        }
+
+
+        static Methods PromptMethod(bool isFolder)
+        {
+            Console.Write("Encrypt or decrypt file{0} ? (E / d)  ", isFolder ? "s in this folder" : "");
+            var input = Console.ReadLine();
+            var result = input == "d" || input == "D" ? Methods.decrypt : Methods.encrypt;
+
+            Console.Write("Chosen method : ");
+            ConsoleColorHelper.WriteLine(result == Methods.encrypt ? "Encrypt" : "Decrypt", ConsoleColorHelper.Colors.Important);
+            return result;
+        }
+
+
+        static bool PromptDestruct(bool isFolder)
+        {
+            Console.Write("\n\nDestruct origin processed file{0} ? (y / N)  ", isFolder ? "s" : "");
+            var input = Console.ReadLine();
+            var result = input == "y" || input == "Y";
+
+            Console.Write("Origin processed file{0} ", isFolder ? "s" : "");
+            ConsoleColorHelper.Write((result ? "WILL" : "WON'T") + " be destroyed", ConsoleColorHelper.Colors.Important);
+            Console.WriteLine(" after processing");
+
+            return result;
+        }
+
+
+        static IEnumerable<string> GetFilesInFolder(string path)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(path);
+            while (queue.Count > 0)
+            {
+                path = queue.Dequeue();
+                foreach (string subDir in Directory.GetDirectories(path))
+                {
+                    queue.Enqueue(subDir);
+                }
+
+                string[] files = null;
+                files = Directory.GetFiles(path);
+                if (files != null)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        yield return files[i];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FileCrypter/Security.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileCrypter
+{
+    public static class Security
+    {
+        static List<byte> defaultByteList = new List<byte> { 222, 199, 144, 79, 31, 43, 133, 28, 80, 75, 128, 50, 227, 95, 11, 208, 96, 232, 99, 69, 176, 16, 111, 64, 178, 41, 19, 125, 159, 89, 251, 0, 217, 25, 182, 214, 169, 158, 5, 241, 154, 1, 149, 127, 150, 86, 109, 122, 56, 141, 242, 163, 130, 101, 20, 218, 181, 206, 119, 55, 126, 33, 198, 138, 66, 220, 124, 44, 123, 196, 216, 168, 116, 6, 240, 134, 37, 106, 174, 54, 204, 36, 211, 226, 51, 100, 243, 197, 102, 255, 12, 65, 237, 221, 3, 186, 71, 10, 229, 201, 114, 58, 250, 13, 209, 207, 108, 78, 115, 188, 67, 225, 177, 85, 73, 239, 235, 97, 203, 81, 27, 234, 249, 151, 254, 104, 238, 59, 148, 228, 120, 92, 172, 87, 62, 152, 49, 143, 175, 53, 91, 244, 84, 103, 189, 145, 230, 179, 74, 135, 26, 8, 194, 45, 90, 252, 219, 167, 34, 63, 40, 18, 129, 146, 22, 72, 2, 24, 113, 82, 161, 14, 93, 131, 88, 17, 121, 4, 170, 202, 215, 21, 147, 236, 192, 185, 245, 60, 160, 180, 46, 223, 118, 30, 42, 164, 184, 157, 173, 117, 153, 29, 187, 213, 94, 70, 136, 39, 23, 183, 166, 165, 224, 52, 98, 35, 107, 195, 233, 190, 200, 156, 212, 140, 205, 162, 15, 142, 247, 137, 110, 83, 191, 139, 57, 155, 76, 61, 132, 253, 9, 77, 193, 47, 38, 68, 48, 248, 231, 32, 210, 7, 246, 112, 105, 171 };
+
+        static string completionStringPercentage;
+
+
+        public static void Encrypt(FileStream fileStream, string newPath, string password)
+        {
+            var substitutionByteList = GetSubstitutionByteList(password);
+            completionStringPercentage = "";
+
+            using (var newFileStream = new FileStream(newPath, FileMode.CreateNew))
+            {
+                var cpt = 0;
+
+                var buffer = new byte[1];
+                while (fileStream.Read(buffer, 0, 1) > 0)
+                {
+                    var index = defaultByteList.IndexOf(buffer[0]);
+                    var encryptedData = new byte[] { substitutionByteList[index] };
+                    newFileStream.Write(encryptedData, 0, 1);
+
+                    DisplayCompletionMeter(cpt, (int)fileStream.Length - 1);
+                    cpt++;
+                }
+            }
+        }
+
+
+        public static void Decrypt(FileStream fileStream, string newPath, string password)
+        {
+            var substitutionByteList = GetSubstitutionByteList(password);
+            completionStringPercentage = "";
+
+            using (var newFileStream = new FileStream(newPath, FileMode.CreateNew))
+            {
+                var cpt = 0;
+
+                var buffer = new byte[1];
+                while (fileStream.Read(buffer, 0, 1) > 0)
+                {
+                    var index = substitutionByteList.IndexOf(buffer[0]);
+                    var decryptedData = new byte[] { defaultByteList[index] };
+                    newFileStream.Write(decryptedData, 0, 1);
+
+                    DisplayCompletionMeter(cpt, (int)fileStream.Length - 1);
+                    cpt++;
+                }
+            }
+        }
+
+
+
+        static List<byte> GetSubstitutionByteList(string password)
+        {
+            var substitutionByteList = Encoding.UTF8.GetBytes(password).ToList();
+            substitutionByteList.AddRange(defaultByteList);
+
+            for (int i = 0; i <= 255; i++)
+            {
+                for(int y = i + 1; y <= 255; y++)
+                {
+                    if(substitutionByteList[i] == substitutionByteList[y])
+                    {
+                        substitutionByteList.RemoveAt(y);
+                        y--;
+                    }
+                }
+            }
+            
+            return substitutionByteList;
+        }
+
+
+        static void DisplayCompletionMeter(int progress, int total)
+        {
+            var completion = ((decimal)progress / (decimal)total) * (decimal)100;
+            var completionToString = completion.ToString();
+            var newCompletionStringPercentage = completionToString.IndexOf(',') != -1 ? completionToString.Substring(0, completionToString.IndexOf(',')) : completionToString;
+
+            if (completion == 1)
+            {
+                Console.Write("\n");
+            }
+            else if(completionStringPercentage == "")
+            {
+                Console.Write("0%");
+                completionStringPercentage = newCompletionStringPercentage;
+            }
+            else if(completionStringPercentage != newCompletionStringPercentage)
+            {
+                for(int i = 0; i < completionStringPercentage.Length + 1; i++)
+                {
+                    Console.Write("\b \b");
+                }
+
+                completionStringPercentage = newCompletionStringPercentage;
+                Console.Write(completionStringPercentage);
+                Console.Write('%');
+            }
+        }
+    }
+}
